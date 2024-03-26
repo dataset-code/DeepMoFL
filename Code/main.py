f20ad32edddf90061ee7f6361ff006fd0b925df1
dataset-code <929570591@qq.com>
2024-03-26 11:53:35 +0800

    update

--- Code/main.py
+++ Code/main.py
@@ -7,59 +7,11 @@ import time
 import os
 import sys
 import json
+import argparse
+
+from compare import compare_classification, compare_regression
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1' 
-
-def compare_classification(expected, actual):
-    # print(expected, actual )
-    if not np.isscalar(actual) and len(actual) == 1 and np.isnan(actual):
-        return False
-    if not np.isscalar(expected) and not np.isscalar(actual) and len(expected) == 1 and len(actual) == 1:
-        expected = expected[0]
-        actual == actual[0]
-    if len(actual.flatten()) == 1:
-        actual = actual.flatten()[0]
-    if len(expected.flatten()) == 1:
-        expected = expected.flatten()[0]
-
-    if np.isscalar(expected) and np.isscalar(actual):
-        return expected == round(actual)
-    if np.isscalar(expected):
-        if len(actual) == 1:
-            return expected == round(actual[0])    
-        return expected == np.argmax(actual)
-    elif np.isscalar(actual):
-        if len(expected) == 1:
-            return expected[0] == round(actual)
-        return np.argmax(expected) == round(actual)
-    if len(expected) != len(actual):
-        print("[????]")
-        return False
-    return np.argmax(expected) == np.argmax(actual)
-
-def compare_regression(expected, actual, delta):
-    if not np.isscalar(expected):
-        expected = expected.flatten()
-    if not np.isscalar(actual):
-        actual = actual.flatten()
-
-    if np.isscalar(expected) and np.isscalar(actual):
-        return abs(expected - actual) <= delta
-    if np.isscalar(expected):
-        if len(actual) > 1:
-            return False
-        return abs(expected - actual[0]) <= delta
-    elif np.isscalar(actual):
-        if len(expected) > 1:
-            return False
-        return abs(expected[0] - actual) <= delta
-    n = len(expected)
-    if n != len(actual):
-        return False
-    res = True
-    for i in range(0, n):
-        res = res and abs(expected[i] - actual[i]) <= delta
-    return res
 
 def calculate_confidence(output, is_classification, expected):
     if is_classification:
@@ -95,7 +47,6 @@ def calculate_confidence(output, is_classification, expected):
         res = np.sqrt(res)
         return res
 
-
 def calculate_neuron_counts(model):
     neuron_counts = []
     for layer in model.layers:
@@ -110,27 +61,21 @@ def select_neuron(neuron_counts, size = 500, seed = 7):
     np.random.seed(seed)
     selected_neuron = []
     new_neuron_counts = []
-    # cur_pos = 0
     for i in neuron_counts:
         if i > size:
             res = sorted(np.random.randint(low=0, high=i, size=size))
-            # res = [j+cur_pos for j in res]
-            # selected_neuron.extend(res)
             selected_neuron.append(res)
             new_neuron_counts.append(size)
         else:
-            # res = [j+cur_pos for j in range(i)]
-            # selected_neuron.extend(res)
             selected_neuron.append([])
             new_neuron_counts.append(i)
-        # cur_pos+=i
     return selected_neuron, new_neuron_counts
 
 
 formula_list = ['tarantula', 'ochiai', 'D_star', 'Op2', 'Barinel']
 
-def main(id, x_test, y_test, model, classification, activation_threshold, res, size = 500, seed = 7, weighted=True):
-    # 计算神经元数量
+def main(x_test, y_test, model, classification, activation_threshold, res, size = 500, seed = 7, weighted=True):
+    # get neuron number
     neuron_counts=calculate_neuron_counts(model)
     print("neuron_counts", neuron_counts, sum(neuron_counts))
     res['neuron_counts'] = neuron_counts
@@ -139,7 +84,7 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
     print("selected_neuron", neuron_counts, sum(neuron_counts))
     res['neuron_counts_selected'] = neuron_counts
 
-    # 执行测试用例
+    # run test cases
     if isinstance(model.layers[0].input_shape, list):
         input_sample = x_test.reshape((len(x_test),)+model.layers[0].input_shape[0][1:])
     else:
@@ -147,17 +92,17 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
 
     layer_outputs = [layer.output for layer in model.layers]
     activation_model = Model(inputs=model.input, outputs=layer_outputs)
-    # print(activation_model.summary())
+    
     if activation_model.layers[0].input_shape[0][0]:
         batch_num = math.ceil(len(x_test)/activation_model.layers[0].input_shape[0][0])
         activations = activation_model.predict(input_sample,steps=batch_num)
     else:
         activations = activation_model.predict(input_sample)
     
-    # 计算激活矩阵
-    coverage_matrix = []    # 每个测试用例的神经元激活情况
+    # calculate activation matrix
+    coverage_matrix = []   
     j = 0
-    for layer_activation in activations:    # 每层的输出（所有测试用例）
+    for layer_activation in activations:    # output of each layer (all test cases)
         new_layer_activation = []
         for i in range(len(x_test)):
             if len(selected_neuron[j]) == 0:
@@ -168,17 +113,16 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
         if isinstance(coverage_matrix,list):
             coverage_matrix = df 
         else:
-            coverage_matrix = pd.concat([coverage_matrix, df],axis = 1,ignore_index=True)   # 拼接输出矩阵，每一行为一个测试用例，每一列为一个神经元
+            coverage_matrix = pd.concat([coverage_matrix, df],axis = 1,ignore_index=True)   # concat row: test case, col: neuron
         j+=1
 
-    coverage_matrix = coverage_matrix.applymap(lambda x: 1 if x > activation_threshold else 0)   # 判断神经元是否激活
+    coverage_matrix = coverage_matrix.applymap(lambda x: 1 if x > activation_threshold else 0)
    
    
-    # 判断测试用例是否通过
-    eva = []    # 测试用例是否通过
+    # ---- test cases confidence ----
+    eva = []    # whether test cases passed 
     confidence = []
     for i in range(len(x_test)):
-        # 取最后一层的输出，跟标签比较，并计算confidence
         if classification:
             if compare_classification(y_test[i], activations[-1][i]):
                 eva.append(1)
@@ -192,11 +136,9 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
 
         confidence.append(calculate_confidence(activations[-1][i], classification, y_test[i]))        
 
-
     coverage_matrix['label']=eva
     coverage_matrix = coverage_matrix.astype(float)
-    # print("【eva】", eva)
-    # print(confidence)
+
     min_val = np.min(confidence)
     max_val = np.max(confidence)
     if min_val == max_val:
@@ -205,25 +147,21 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
         confidence = (confidence - min_val) / (max_val - min_val)
     if not classification:
         confidence = [1-x for x in confidence]
-    # confidence = [min(x+1e-8, 1) for x in confidence]
-    # print("【confidence】", confidence)
-    # print(activations[-1])
-    # print(coverage_matrix)
+
     if not weighted:
         confidence = [0 for _ in confidence]
 
-    # ---- 计算每个神经元的可疑值 ----
+    # ---- calculate neuron suspiciousness ----
     neuron_score_list = {}
     pass_fail = []
 
-    total_pass = 1e-4 # 避免除0错误
+    total_pass = 1e-4 
     total_fail = 1e-4
     for i in range(len(eva)):
         if eva[i] == 1:
             total_pass+=(1+confidence[i])
         else:
             total_fail+=(1+confidence[i])
-    print(f"total_pass and fail")
 
     for i in range(sum(neuron_counts)):
         passed=0
@@ -240,10 +178,8 @@ def main(id, x_test, y_test, model, classification, activation_threshold, res, s
                 neuron_score_list[i] = [calculate_suspiciousness(passed, failed, total_pass, total_fail, i)]
             else:
                 neuron_score_list[i].append(calculate_suspiciousness(passed, failed, total_pass, total_fail, i))
-
-    # print("【neuron suspiousness】",neuron_score_list)
-    # ---- 计算每一层的可疑值 ----
-    # 取每个神经元的平均值作为每一层的可疑值
+                
+    # ---- calculate layer suspiciousness ----
     cur_count = 0
 
     layer_score_list = {}
@@ -283,6 +219,8 @@ def calculate_suspiciousness(passed, failed, total_pass, total_fail, formula):
 
 def aggregate(score_list):
     score = np.mean(score_list)*(1-gini_coefficient(np.array(score_list)))
+    if np.isnan(score):
+        score = 0
     return score   
 
 def gini_coefficient(x):
@@ -292,31 +230,6 @@ def gini_coefficient(x):
         diffsum += np.sum(np.abs(xi - x[i:]))
     return diffsum / (len(x)**2 * np.mean(x))
 
-
-def run(id, is_classification, selected_neuron_num, activation_threshold, seed):
-
-    x_test = np.load(f"../Dataset/{id}/x_test.npy")
-    y_test = np.load(f"../Dataset/{id}/y_test.npy")
-    model = keras.models.load_model(f"../Dataset/{id}/model.h5")
-
-    res = {}
-    t1 = time.time()
-    main(id, x_test, y_test, model, is_classification, activation_threshold, res, selected_neuron_num, seed)
-    t2 = time.time()
-    res['time'] = t2-t1
-    print(f"time: {t2-t1}")
-
-    # print(res)
-    with open(f'res_{activation_threshold}_{selected_neuron_num}_{seed}_gini.json',"w",encoding='utf-8') as f:
-        json.dump(res,f)
-
-if __name__ == "__main__":
-    id = sys.argv[1]
-    is_classification = sys.argv[2]
-    selected_neuron_num = int(sys.argv[3])
-    activation_threshold = float(sys.argv[4])
-    seed = int(sys.argv[5])
-    run(id, is_classification, selected_neuron_num, activation_threshold, seed)
 
 
 
--- README.md
+++ README.md
@@ -12,22 +12,31 @@ We selected 111 model bugs that were reproducible.
 Second, we used the SQL query provided by [Deepmufl](https://github.com/ali-ghanbari/deepmufl-ase-2023) and added some rules to obtain StackOverflow posts from Stack Exchange Data Explorer and then filtered model bugs from them. The query is in `Dataset/query.sql` and the file `Dataset/QueryResults.csv` contains the raw result of running the SQL query as of Januray 2024, which contains 240 eligible StackOverflow posts. We further manually filtered model bugs and collected a total of 10 bugs.
 
 ### Bugs
-The `Dataset/bugs` folder contains 121 model bugs used in our experiment, with each error corresponding to a Stack Overflow post ID. In the respective folders, there are source codes required to train the buggy model.
+The `Dataset/bugs` folder contains 121 model bugs used in our experiment, with each bug corresponding to a Stack Overflow post ID. In the respective folders, there are source codes required to train the buggy model.
 
 The `dataset.csv` file contains basic information about each bug, including Stack Overflow post ID, whether it is a classification model, the number of error layers, and error type.
 
 
 ## Source Code
+We've tested our code on windows10 and ubuntu 22.04 with python 3.10.
+
+### Requirments
+ - tensorflow == 2.9.0
+ - pandas == 2.2.1
+
+### Run
 The `Code` folder contains the source code for our method. You can run it from the command line as follows:
 ```
-python main.py <id> <is_classification> <selected_neuron_num> <activation_threshold> <seed>
+python run.py -m MODEL -i INPUT -o OUTPUT -c CLASSIFICATION -n SELECTED_NEURON_NUM -t THRESHOLD -s SEED
 ```
 There are five parameters:
- - `id`: bug id in our dataset
- - `is_classification`: whether the buggy model is a classification model
- - `selected_neuron_num`: the number of selected neurons
- - `activation_threshold`: the activation threshold used to determine whether a neuron is activated
- - `seed`: random number seeds when randomly selecting neurons
+ - `MODEL`: The file path of the model, which needs to be .h5 file
+ - `INPUT`: The file path to the test case inputs, which needs to be a .npy file
+ - `OUTPUT`: The file path to the test case outputs, which needs to be a .npy file
+ - `CLASSIFICATION`: whether the buggy model is a classification model (If it's a classification model, pass 1. If it's a regression model, pass 0)
+ - `SELECTED_NEURON_NUM`: the number of selected neurons
+ - `SELECTED_NEURON_NUM`: the activation threshold used to determine whether a neuron is activated
+ - `SEED`: random number seeds when randomly selecting neurons
 
 ## Results
 The `results` folder contains the full results of our experiment.
